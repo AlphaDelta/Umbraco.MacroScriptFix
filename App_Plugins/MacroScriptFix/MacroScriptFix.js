1cfaf48c8284829013037c45ef4724ba7e4c480c
AlphaDelta <admin@shaw.co.nz>
2021-07-01 15:56:52 +1200

    Initial commit

---
+++ App_Plugins/MacroScriptFix/MacroScriptFix.js
@@ -0,0 +1,56 @@
+﻿angular.module("umbraco")
+    .controller("Umbraco.PropertyEditors.Grid.MacroController",
+        function ($scope, $timeout, editorService, macroResource, macroService, $routeParams, $sce) {
+            $scope.title = "Click to insert macro";
+
+            $scope.setMacro = function () {
+
+                var dialogData = {
+                    richTextEditor: true,
+                    macroData: $scope.control.value || {
+                        macroAlias: $scope.control.editor.config && $scope.control.editor.config.macroAlias
+                            ? $scope.control.editor.config.macroAlias : ""
+                    }
+                };
+
+                var macroPicker = {
+                    dialogData: dialogData,
+                    submit: function (model) {
+                        var macroObject = macroService.collectValueData(model.selectedMacro, model.macroParams, dialogData.renderingEngine);
+
+                        $scope.control.value = {
+                            macroAlias: macroObject.macroAlias,
+                            macroParamsDictionary: macroObject.macroParamsDictionary
+                        };
+
+                        $scope.setPreview($scope.control.value);
+                        editorService.close();
+                    },
+                    close: function () {
+                        editorService.close();
+                    }
+                }
+                editorService.macroPicker(macroPicker);
+            };
+
+            $scope.setPreview = function (macro) {
+                var contentId = $routeParams.id;
+
+                macroResource.getMacroResultAsHtmlForEditor(macro.macroAlias, contentId, macro.macroParamsDictionary)
+                    .then(function (htmlResult) {
+                        $scope.title = macro.macroAlias;
+                        if (htmlResult.trim().length > 0 && htmlResult.indexOf("Macro:") < 0) {
+                            $scope.preview = $sce.trustAsHtml(htmlResult);
+                        }
+                    });
+
+            };
+
+            $timeout(function () {
+                if ($scope.control.$initializing) {
+                    $scope.setMacro();
+                } else if ($scope.control.value) {
+                    $scope.setPreview($scope.control.value);
+                }
+            }, 200);
+        });
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Umbraco.MacroScriptFix
 Allows macros to include style/link/script tags
+
+## Usage
+Simply copy the `MacroScriptFix` directory into your `App_Plugins`.
+The fix will then be run on the next startup.
+
+## Preview
+<img src="https://i.imgur.com/LP6JLBN.png" />
+<img src="https://i.imgur.com/gDeaUbt.png" />
+
+## License
+This repo is licensed under [CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0/).
+All rights wronged, all wrongs reversed.
